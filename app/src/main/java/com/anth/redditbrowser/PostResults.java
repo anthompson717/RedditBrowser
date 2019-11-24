161364fe8f649797bca791f149f6c76a83558127
jdcorona96 <josed.hc96@gmail.com>
2019-11-24 16:13:41 -0700

    setting up Post Results Activity for results

--- app/src/main/java/com/anth/redditbrowser/PostResults.java
+++ app/src/main/java/com/anth/redditbrowser/PostResults.java
@@ -3,6 +3,10 @@ package com.anth.redditbrowser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PostResults extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class PostResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_results);
+        //setAdapter();
+    }
+
+    private void setAdapter() {
+
+        final ListView postListview = (ListView) findViewById(R.id.post_results);
+
+        ArrayList<HashMap<String, String>> postList = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> hash = new HashMap<String, String>();
+
+        String[] from = { }; //TODO: set the hash keys
+        int[] to = { }; // TODO: set the layout_id names (some are set up but
+                        //       feel to change them or add more on adapter_post_item.xml
+
+        //TODO: set the content of the list & hashmap to place on adapter
+        /*
+        for(int i=0; i< ; i++) // TODO: set the size
+        {
+            hash.put(); // TODO: set the contents of Hashmap
+            postList.add(hash);
+        }
+
+        postListView.setAdapter(postList);
+
+        */
     }
 }

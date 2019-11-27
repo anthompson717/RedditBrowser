b0b4511fa30e51ceef21ea8ca96f8262e20e39a7
Alexander <anthompson@email.arizona.edu>
2019-11-26 18:18:40 -0700

    Started Login Process changed activity to fragment

--- app/src/main/java/com/anth/redditbrowser/MainActivity.java
+++ app/src/main/java/com/anth/redditbrowser/MainActivity.java
@@ -1,6 +1,8 @@
 package com.anth.redditbrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        LogInFragment fragment = new LogInFragment();
+        fragmentTransaction.add(R.id.outer, fragment);
+        fragmentTransaction.commit();
     }
 
     public void search(View v) {

23136c58f55289da6c661743978c63bb4ad45c21
anthompson717 <anthompson@email.arizona.edu>
2019-12-01 20:57:01 -0700

    Subreddit search up and running

--- app/src/main/java/com/anth/redditbrowser/MainActivity.java
+++ app/src/main/java/com/anth/redditbrowser/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,22 +15,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);/*
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         LogInFragment fragment = new LogInFragment();
         fragmentTransaction.add(R.id.outer, fragment);
-        fragmentTransaction.commit();
+        fragmentTransaction.commit();*/
     }
 
     public void search(View v) {
+        EditText insert = findViewById(R.id.search_query);
+        String search = insert.getText().toString();
 
         RadioButton radio = (RadioButton) findViewById(R.id.radio_subreddit);
         boolean isSubreddit = radio.isChecked();
 
         Intent searchResult;
-        if (isSubreddit)
+        if (isSubreddit) {
             searchResult = new Intent(this, SubRedditResults.class);
+            searchResult.putExtra("search", search);
+            startActivity(searchResult);
+        }
         else
             searchResult = new Intent(this, PostResults.class);
 
